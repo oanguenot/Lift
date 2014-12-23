09e7522e1f45e70f4a14784a7c29aca526bc83d9
unknown <angueno1@FRILLN0H476652.emea.lucent.com>
2014-12-23 18:54:13 +0100

    Complete display of a conference. Actions to do

--- src/views/conferenceView.js
+++ src/views/conferenceView.js
@@ -10,6 +10,10 @@ define('views/conferenceView', ['text!views/templates/conference.html'], functio
         },
 
         events: {
+            'click .meeting-join-button': 'onJoin',
+            'click .meeting-details-button': 'onDetails',
+            'click .meeting-edit-button': 'onEdit',
+            'click .meeting-remove-button': 'onRemove'
         },
 
         render: function() {
@@ -20,6 +24,46 @@ define('views/conferenceView', ['text!views/templates/conference.html'], functio
 
             this.$('.meeting-state').addClass('meeting-' + this.model.get('state'));
             this.$('.meetingTitle').text(this.model.get('subject'));
+            if(this.model.get('isAnInvite')) {
+                this.$('.meetingState').html('<small>Invite from </small>' + this.model.get('owner'));
+            }
+            else {
+                this.$('.meetingState').text(this.model.get('stateDisplayed'));
+            }
+
+            if(this.model.get('typeConf') === "scheduled") {
+                this.$('.meetingTime').text(this.model.get('startTime') + " - " + this.model.get('endTime'));
+                this.$('.meetingTimezone').text(this.model.get('timezone'));
+            }
+            else {
+                this.$('.meetingTime').text('Whole day');
+                if (this.model.get('days') > 30) {
+                    this.$('.meetingTimezone').text('> 1 month left');
+                }
+                else if (this.model.get('days') > 1) {
+                    this.$('.meetingTimezone').text(this.model.get('days') + " days left");
+                }
+                else if (this.model.get('days') === 1) {
+                    this.$('.meetingTimezone').text(this.model.get('days') + " day left");
+                }
+                else if (this.model.get('days') === 0) {
+                    this.$('.meetingTimezone').text("Expires today");
+                }
+                else {
+                    this.$('.meetingTimezone').text("");
+                }
+            }
+
+            this.$('.meeting-join-button').attr("id","join-" + this.model.get('vanity'));
+            this.$('.meeting-details-button').attr("id", "details-" + this.model.get('vanity'));
+            this.$('.meeting-edit-button').attr("id", "edit-" + this.model.get('vanity'));
+            this.$('.meeting-remove-button').attr("id", "remove-" + this.model.get('vanity'));
+
+            if(this.model.get('isAnInvite')) {
+                this.$('.meeting-edit-button').addClass('.meeting-edit-button-disabled');
+                this.$('.meeting-remove-button').addClass('.meeting-remove-button-disabled');
+            }
+
 
             return this;
         },
@@ -29,6 +73,22 @@ define('views/conferenceView', ['text!views/templates/conference.html'], functio
             this.undelegateEvents();
             this.unbind();
             this.off();
+        },
+
+        onRemove: function() {
+
+        },
+
+        onEdit: function() {
+
+        },
+
+        onDetails: function() {
+
+        },
+
+        onJoin: function() {
+            
         }
     });
 });
